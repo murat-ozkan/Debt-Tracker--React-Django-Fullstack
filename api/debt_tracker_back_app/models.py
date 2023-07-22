99815fd94cae250a933c083b302ec28971c512bf
muratozkan79 <muratozkan79@gmail.com>
2023-07-22 21:10:07 +0300

    serializers added

--- api/debt_tracker_back_app/models.py
+++ api/debt_tracker_back_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Debt(models.Model):
+    toWhom = models.CharField(max_length=50)
+    howMuch = models.FloatField(max_length=20)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.toWhom
---
+++ api/debt_tracker_back_app/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Debt
+
+
+class DebtSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Debt
+        fields = "__all__"
+
+        # Can make validation here.
